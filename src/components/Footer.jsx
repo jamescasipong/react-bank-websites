c7bd8132dbcee2eea2407bb5f664c5339d172a8f
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-06-29 00:01:26 +0800

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;

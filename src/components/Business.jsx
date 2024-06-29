67d54a104ad42e1a91570871bf253b4225a88553
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-06-30 00:46:23 +0800

    sd

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -22,7 +22,7 @@ const FeatureCard = ({icon, title, content, index}) => (
 const Business = () => {
   
   return (
-    <section id="features" className={layout.section}>
+    <section id="features" className={layout.ssction}>
         <div className={layout.sectionInfo}>
           <h2 className={styles.heading2}>You do the business, we’ll handle the money.</h2>
           <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, 

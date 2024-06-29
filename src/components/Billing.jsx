df18498c4b3670bedaac39749b56886da6da9aff
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-06-30 00:46:04 +0800

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -5,7 +5,7 @@ import styles, { layout } from "../styles";
 const Billing = () => {
   return (
     
-    <section id="product" className={`${layout.sectionReverse} `}>
+    <section id="product" className={`${layout.sectionReverse}`}>
       <div className={layout.sectionImgReverse}>
           <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
 
@@ -15,7 +15,7 @@ const Billing = () => {
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div>
       </div>
 
-      <div className="flex flex-1 flex-col justify-center p-10">
+      <div className="flex flex-1 flex-col justify-end sm:p-[60px] p-[5px]">
         <div className="flex flex-col">
         <h2 className={`${styles.heading2}`}>Easily control your<br className="sm:block hidden"/> billing & invoicing.</h2>
         <p className="font-poppins font-normal text-dimWhite 
@@ -23,9 +23,9 @@ const Billing = () => {
         ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
 
         </div>
-        <div className="flex flex-1 flex-row mt-5 gap-5">
-        <img src={google} alt="bill" className="w-[128.86px] h-[42.05px] object-contain"/>
-        <img src={apple} alt="bill" className="w-[128.86px] h-[42.05px] object-contain"/>
+        <div className="flex flex-row sm:m-10 mt-6 gap-5">
+        <img src={google} alt="google_play" className="w-[128px] h-[42px] object-contain cursor-pointer"/>
+        <img src={apple} alt="apple" className="w-[128px] h-[42px] object-contain cursor-pointer"/>
         </div>
 
       </div>

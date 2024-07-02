580bd7c61408a6ab55275cdb795b06a16875a2f9
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-07-02 17:07:24 +0800

    asd

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Add all changes to the staging area
 git add .
-git commit -m "Auto commit $(Get-Date)"
+
+# Commit changes with a default message
+git commit -m "Automatic commit triggered by save"
+
+# Push changes to remote repository (optional, adjust according to your needs)
 git push origin main  # Replace 'main' with your branch name if different
--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,17 +4,17 @@ import Button from "./Button";
 
 const CTA = () => (
   
-  <section className={`my-6 flex justify-center items-center relative w-full pt-[40px] pl-[60px] pb-[40px] pr-[60px] bg-black-gradient-2 rounded-xl`}>
+  <section className={`my-6 flex sm:flex-row flex-col justify-center items-center relative w-full pt-[40px] sm:pl-[60px] pl-[24px] sm:pb-[40px] pb-[20px] sm:pr-[60px] pr-[24px] bg-black-gradient-2 rounded-xl`}>
     <div className={`${layout.sectionInfo}`}>
-      <h2 className={`${styles.heading2}`}>
+      <h2 className={`${styles.heading2} sm:text-left text-center`}>
       Let’s try our service now!
       </h2>
-      <p className={`${styles.paragraph} mt-5`}>
-      Everything you need to accept card payments<br /> and grow your business anywhere on the planet.
+      <p className={`${styles.paragraph} mt-5 sm:max-w-auto max-w-[450px] sm:text-left text-center`}>
+      Everything you need to accept card payments<br className="sm:block hidden"/> and grow your business anywhere on the planet.
       </p>
     </div>
 
-    <div className={`flex justify-end items-center`}>
+    <div className={`flex sm:justify-end justify-center items-center sm:mt-0 mt-10`}>
       <Button styles="rounded-md"></Button>
     </div>
   </section>
